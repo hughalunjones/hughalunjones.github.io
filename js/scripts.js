1c6d5db34230ce205d965d6ab05c62fce50b2fb8
Hugh Alun-Jones <hughalunjones12@gmail.com>
2021-02-17 20:11:24 +0000

    Reworked stylesheet to conform to "Mobile first". Implemented a new nav.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("hello");
+menuToggler.addEventListener('click', ev => {
+  menuToggler.classList.toggle('open');
+});
+
+for (const element of document.querySelectorAll("nav a")){
+  element.addEventListener("click", ev => (
+    menuToggler.classList.remove("open")
+  ));
+}
