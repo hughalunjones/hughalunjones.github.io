1b98c96ee38185852f2eb514ed8d95b7675cdaf5
hughalunjones <58294770+hughalunjones@users.noreply.github.com>
2021-06-02 16:05:19 +0100

    Fixed some minor indentation issues for consistency

--- js/projects.js
+++ js/projects.js
@@ -27,13 +27,13 @@ const projectData = [
 
 function getQueryVariable(variable)
 {
-       let query = window.location.search.substring(1);
-       let vars = query.split("&");
-       for (let i = 0;i < vars.length;i++) {
-               let pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
-       }
-       return(false);
+    let query = window.location.search.substring(1);
+    let vars = query.split("&");
+    for (let i = 0;i < vars.length;i++) {
+       let pair = vars[i].split("=");
+       if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
 }
 let projectIndex = getQueryVariable("id");
 
